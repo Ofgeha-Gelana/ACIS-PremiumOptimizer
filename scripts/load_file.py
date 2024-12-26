b344b6c57f83ef285f65353a3f4bdb8ff4e77387
Ofgeha Gelana <ofgehagelana2019@gmail.com>
2024-12-26 11:09:59 +0300

    adding function to load data

--- scripts/load_file.py
+++ scripts/load_file.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def load_data(filename):
     # Load the dataset
-    file_path = 'path_to_your_file.txt'  # Replace with the actual path to your file
+    file_path = '../data/raw/MachineLearningRating_v3.txt'  # Replace with the actual path to your file
     df = pd.read_csv(file_path, delimiter='|')
 
     # Display the first few rows of the DataFrame
-    print(df.head())
+    return df
